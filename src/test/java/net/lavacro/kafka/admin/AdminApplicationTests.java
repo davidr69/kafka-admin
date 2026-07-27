bf0c408eb8e83c65204dad4572669c831b30932c
David Rosario <david@lavacro.com>
2026-07-27 18:52:20 -0400

    initial check-in

---
+++ src/test/java/net/lavacro/kafka/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lavacro.kafka.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
